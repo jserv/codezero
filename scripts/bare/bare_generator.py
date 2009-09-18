99ebb7c50deb3ac470d5e856f18639934164504d
Bahadir Balban <bbalban@b-labs.co.uk>
2009-09-18 18:49:56 +0300

    Bare container correctly autogenerates from `test' sources and builds. 	new file:   conts/test/SConstruct 	new file:   conts/test/container.c 	new file:   conts/test/include/linker.lds 	new file:   conts/test/include/test.h 	new file:   conts/test/main.c 	new file:   conts/test/src/test.c 	modified:   scripts/bare/bare_generator.py

--- scripts/bare/bare_generator.py
+++ scripts/bare/bare_generator.py
@@ -22,6 +22,11 @@ class BareContGenerator:
     def __init__(self):
         self.CONT_SRC_DIR = ''   # Set when container is selected
         self.BARE_SRC_BASEDIR = join(PROJROOT, 'conts')
+        self.EXAMPLE_PROJ_SRC_DIR = join(PROJROOT, 'conts/test')
+
+        self.main_builder_name = 'build.py'
+        self.main_configurator_name = 'configure.py'
+        self.mailing_list_url = 'http://lists.l4dev.org/mailman/listinfo/codezero-devel'
 
         self.build_script_in = join(SCRIPTROOT, 'SConstruct.in')
         self.build_readme_in = join(SCRIPTROOT, 'build.readme.in')
@@ -29,20 +34,16 @@ class BareContGenerator:
 
         self.build_script_name = 'SConstruct'
         self.build_readme_name = 'build.readme'
-        self.build_desc_name = 'container.description'
+        self.build_desc_name = 'cont.desc'
 
         self.build_script_out = None
         self.build_readme_out = None
         self.build_desc_out = None
-        self.main_builder_name = 'build.py'
-        self.main_configurator_name = 'configure.py'
-        self.mailing_list_url = 'http://lists.l4dev.org/mailman/listinfo/codezero-devel'
+        self.src_main_out = None
 
-    def create_bare_dirtree(self, config, cont):
-        # First, create the container directory structure
-        os.mkdir(self.CONT_SRC_DIR)
-        os.mkdir(join(self.CONT_SRC_DIR, 'src'))
-        os.mkdir(join(self.CONT_SRC_DIR, 'include'))
+    def create_bare_srctree(self, config, cont):
+        # First, create the base project directory and sources
+        shutil.copytree(self.EXAMPLE_PROJ_SRC_DIR, self.CONT_SRC_DIR)
 
     def copy_bare_build_desc(self, config, cont):
         with open(self.build_desc_in) as fin:
@@ -63,34 +64,21 @@ class BareContGenerator:
                                   self.main_configurator_name, \
                                   self.main_configurator_name))
 
-    def copy_bare_build_script(self, config, cont):
-        with open(self.build_script_in) as fin:
-            str = fin.read()
-            with open(self.build_script_out, 'w+') as fout:
-                # Make any manipulations here
-                fout.write(str)
-
     def create_bare_sources(self, config, cont):
         # Determine container directory name.
         self.CONT_SRC_DIR = join(self.BARE_SRC_BASEDIR, cont.dirname.lower())
-        print "container source dir: " + self.CONT_SRC_DIR
 
-        self.build_script_out = join(self.CONT_SRC_DIR, self.build_script_name)
         self.build_readme_out = join(self.CONT_SRC_DIR, self.build_readme_name)
         self.build_desc_out = join(self.CONT_SRC_DIR, self.build_desc_name)
 
-        self.create_bare_dirtree(config, cont)
-        self.copy_bare_build_script(config, cont)
+        self.create_bare_srctree(config, cont)
         self.copy_bare_build_readme(config, cont)
         self.copy_bare_build_desc(config, cont)
-        #copy_bare_sources(config, cont)
 
     def check_create_bare_sources(self, config):
         for cont in config.containers:
             if cont.type == "bare":
-                print "Cont type: " + cont.type
                 if not os.path.exists(join(self.BARE_SRC_BASEDIR, cont.dirname)):
-                    print "Creating new at " + join(self.BARE_SRC_BASEDIR, cont.dirname)
                     self.create_bare_sources(config, cont)
 
     def bare_container_generate(self, config):
