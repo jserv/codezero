00adcd9afaafdafb57d2d371b52cfe94dcce206a
Bora Sahin <bora.sahin@b-labs.co.uk>
2009-10-29 22:00:38 +0200

    libl4thread skeleton is introduced.
    With this library, it is aimed at easing thread manipulation, utcb handling etc.

---
+++ conts/libl4thread/include/l4thread/thread.h
@@ -0,0 +1,9 @@
+/*
+ *
+ */
+#ifndef __LIB_THREAD_H__
+#define __LIB_THREAD_H__
+
+void l4thread_print(void);
+
+#endif /* __LIB_THREAD_H__ */
---
+++ conts/libl4thread/src/thread.c
@@ -0,0 +1,10 @@
+/*
+ *
+ */
+
+#include <stdio.h>
+
+void l4thread_print(void)
+{
+	printf("Hello world from libl4thread!\n");
+}
